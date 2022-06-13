813899fbdc2b51e6db1a18d61db4262790611b35
lyuvvi <lyuba_gaydash@mail.ru>
2022-06-13 16:49:18 +0300

    исправление ошибок

--- js/authorization.js
+++ js/authorization.js
@@ -1,8 +1,9 @@
-allDiv=3;
-function showDv(n){
-    for(i=1;allDiv+1>i;i++){
-    document.getElementById('d'+i).style.display = 'none';
-    }
-    document.getElementById('d'+n).style.display = 'flex';
+allDiv = 3;
+function showDv(n) {
+  for (i = 1; allDiv + 1 > i; i++) {
+    document.getElementById('d' + i).style.display = 'none';
+  }
+  document.getElementById('d' + n).style.display = 'flex';
+  return false;
 }
 
