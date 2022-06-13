0e606e49f35f2a4b0f88992dc7075eed5286cf73
lyuvvi <lyuba_gaydash@mail.ru>
2022-06-13 16:14:20 +0300

    добавление перехода по нажатию на ссылку, исправить ошибку (все работает, но происходит "перезагрузка" страницы)

--- js/authorization.js
+++ js/authorization.js
@@ -1,17 +1,8 @@
-function myFunction() {
-    document.getElementById("account").classList.toggle("show");
-}
-
-window.onclick = function(event) {
-  if (!event.target.matches('.header__click')) {
-
-    var dropdowns = document.getElementsByClassName("header__content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+allDiv=3;
+function showDv(n){
+    for(i=1;allDiv+1>i;i++){
+    document.getElementById('d'+i).style.display = 'none';
     }
-  }
+    document.getElementById('d'+n).style.display = 'flex';
 }
+
