06583a957b4454e6fcc3f2cb6152c7332e7cba98
lyuvvi <lyuba_gaydash@mail.ru>
2022-07-01 19:50:35 +0300

    верстка модального окна, для создания темы

--- js/index.js
+++ js/index.js
@@ -15,3 +15,24 @@ window.onclick = function (event) {
     }
   }
 }
+
+// Модальное окно
+$('#discussions__modal').click(function (e) {
+  e.preventDefault();
+  $('.discussions__window').addClass('discussions__window_active');
+  $('body').addClass('hidden');
+});
+
+$('.modal__close-button').click(function (e) {
+  e.preventDefault();
+  $('.discussions__window').removeClass('discussions__window_active');
+  $('body').removeClass('hidden');
+});
+
+$('.discussions__window').mouseup(function (e) {
+  let modalContent = $(".discussions__content");
+  if (!modalContent.is(e.target) && modalContent.has(e.target).length === 0) {
+    $(this).removeClass('discussions__window_active');
+    $('body').removeClass('hidden');
+  }
+});
